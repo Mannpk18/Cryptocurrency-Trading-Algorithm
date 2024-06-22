13187239548e3f77ff7fb17a12ea48ed26ff1093
Mannpk18 <144070096+Mannpk18@users.noreply.github.com>
2024-06-22 14:24:49 +0300

    Update CryptoProject.py

--- CryptoProject.py
+++ CryptoProject.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.dates import DateFormatter
-BTC_USD = yf.download("BTC-USD", start='2023-01-01', end='2023-12-31', interval='1d')
+BTC_USD = yf.download("BTC-USD", start='2020-01-01', end='2023-12-31', interval='1d')
 BTC_USD.head()
 fig, ax = plt.subplots(dpi=500)
 
